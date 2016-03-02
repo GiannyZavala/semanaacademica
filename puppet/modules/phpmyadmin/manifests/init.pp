74f615a83bede16b6987dfb5bb7f8d590fcfd2bd
Cristian D. Velazquez Ramirez <cristian@wizeservices.com>
2016-03-02 12:10:52 -0600

    Move harcoded path (www) to dynamic (wwwroot)

--- puppet/modules/phpmyadmin/manifests/init.pp
+++ puppet/modules/phpmyadmin/manifests/init.pp
@@ -4,7 +4,7 @@ class phpmyadmin {
     require => Package['php5-fpm'],
   }
 
-  file { '/www/phpmyadmin':
+  file { "$::wwwroot/phpmyadmin":
     ensure  => 'link',
     target  => '/usr/share/phpmyadmin',
     require => Package['phpmyadmin'],
